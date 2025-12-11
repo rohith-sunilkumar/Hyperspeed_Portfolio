176cef25af990d1750861749a04140534b7d9a88
rohith-sunilkumar <rohith.sunilkumar2020@gmail.com>
2025-12-11 12:49:12 +0530

    Added sitemap plugin

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ViteSitemapPlugin } from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    ViteSitemapPlugin({
-      hostname: 'https://rohithsunilkumar.netlify.app',
-    }),
-  ],
+  plugins: [react()],
 })
