702145713577dd4de100724212e7952151b3753a
rohith-sunilkumar <rohith.sunilkumar2020@gmail.com>
2025-12-14 11:19:48 +0530

    Remove cinematic intro and hyperspeed background animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from 'react';
+import { Fragment } from 'react';
 import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
@@ -7,71 +7,21 @@ import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
-import Hyperspeed from './components/Hyperspeed/Hyperspeed.jsx';
-import CinematicIntro from './components/CinematicIntro';
 
 function App() {
-  const [showIntro, setShowIntro] = useState(true);
-
   return (
     <Fragment>
-      {showIntro ? (
-        <CinematicIntro onComplete={() => setShowIntro(false)} />
-      ) : (
-        <Fragment>
-          <Hyperspeed
-            effectOptions={{
-              onSpeedUp: () => { },
-              onSlowDown: () => { },
-              distortion: 'turbulentDistortion',
-              length: 400,
-              roadWidth: 10,
-              islandWidth: 2,
-              lanesPerRoad: 4,
-              fov: 90,
-              fovSpeedUp: 150,
-              speedUp: 2,
-              carLightsFade: 0.4,
-              totalSideLightSticks: 20,
-              lightPairsPerRoadWay: 40,
-              shoulderLinesWidthPercentage: 0.05,
-              brokenLinesWidthPercentage: 0.1,
-              brokenLinesLengthPercentage: 0.5,
-              lightStickWidth: [0.12, 0.5],
-              lightStickHeight: [1.3, 1.7],
-              movingAwaySpeed: [60, 80],
-              movingCloserSpeed: [-120, -160],
-              carLightsLength: [400 * 0.03, 400 * 0.2],
-              carLightsRadius: [0.05, 0.14],
-              carWidthPercentage: [0.3, 0.5],
-              carShiftX: [-0.8, 0.8],
-              carFloorSeparation: [0, 5],
-              colors: {
-                roadColor: 0x080808,
-                islandColor: 0x0a0a0a,
-                background: 0x000000,
-                shoulderLines: 0xFFFFFF,
-                brokenLines: 0xFFFFFF,
-                leftCars: [0xD856BF, 0x6750A2, 0xC247AC],
-                rightCars: [0x03B3C3, 0x0E5EA5, 0x324555],
-                sticks: 0x03B3C3,
-              }
-            }}
-          />
-
-          {/* Main Content */}
-          <div className="min-h-screen relative">
-            <Navbar />
-            <Hero />
-            <About />
-            <Skills />
-            <Education />
-            <Experience />
-            <Projects />
-            <Contact />
-          </div>
-        </Fragment>
-      )}
+      {/* Main Content */}
+      <div className="min-h-screen relative">
+        <Navbar />
+        <Hero />
+        <About />
+        <Skills />
+        <Education />
+        <Experience />
+        <Projects />
+        <Contact />
+      </div>
     </Fragment>
   );
 }
